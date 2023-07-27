21e32918623e9d9e72429a46ef01ba1e1849c784
Simon125q <sz.omiecinski@gmail.com>
2023-07-27 21:37:45 +0200

    create gif class, extend image class

--- ascii_converter.py
+++ ascii_converter.py
@@ -2,12 +2,18 @@ import sys
 from PIL import Image
 
 class ASCII_image:
-    def __init__(self, image_path, new_width = 100):
+    def __init__(self, image_path = None, image = None, new_width = None):
         self.ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
-        self.image_path = image_path
         self.new_width = new_width
-        self.open_image()
-        if self.new_width != 0:
+        if image_path and not image:
+            self.image_path = image_path
+            self.open_image()
+        elif image and not image_path:
+            self.image = image
+        else:
+            print('Provide only path or image')
+            exit(1)
+        if self.new_width != None:
             self.resize_image()
         else:
             self.new_width, self.new_height = self.image.size
@@ -22,7 +28,7 @@ class ASCII_image:
             self.image = Image.open(self.image_path)
         except:
             print(self.image_path, " is not a valid path to an image.")
-            sys.exit(0)
+            sys.exit(2)
         
     def resize_image(self):
         width, height = self.image.size
@@ -49,8 +55,41 @@ class ASCII_image:
             with open(file_name.split('.')[:-1] + "_ascii_image.txt", "w") as f:
                 f.write(self.ascii_image)
         
+class ASCII_gif():
+    def __init__(self, gif_path, new_width=100):
+        self.ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+        self.gif_path = gif_path
+        self.new_width = new_width
+        self.gif_frames = []
+        self.open_gif()
+        self.convert_to_ascii()
+        print(len(self.gif_frames))
+        print(self.gif_frames)
+        
+    def open_gif(self):
+        try:
+            with Image.open(self.gif_path) as gif:
+                try:
+                    while True:
+                        gif.seek(gif.tell() + 1)
+                        self.gif_frames.append(gif)
+                except:
+                    pass
+        except:
+            print(self.gif_path, " is not a valid path to an gif.")
+            sys.exit(0)
+            
+    def convert_to_ascii(self):
+        converted_frames = []
+        for frame in self.gif_frames:
+            frame = ASCII_image(frame)
+            converted_frames.append
+    
+    
+    
         
 if __name__ == "__main__":
-    path = "./mario.png"
-    ascii_img = ASCII_image(path)
-    ascii_img.print_image()
+    # path = "./mario.png"
+    # ascii_img = ASCII_image(path)
+    # ascii_img.print_image()
+    gif = ASCII_gif("C:/Users/szomi/Dropbox/Komputer/Desktop/simson.gif")
